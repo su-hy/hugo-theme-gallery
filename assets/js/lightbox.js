ca6fc0ca640aa4398947fa83c429bc671ee08557
su-hy <suchyy@gmail.com>
2025-04-08 22:25:14 +0200

    x

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -45,6 +45,32 @@ if (gallery) {
     });
   }
 
+  // Add fullscreen button
+  lightbox.on("uiRegister", () => {
+    lightbox.pswp.ui.registerElement({
+      name: "fullscreen-button",
+      order: 9,
+      isButton: true,
+      html: {
+        isCustomSVG: true,
+        // Adjusted SVG path to match other icons' alignment
+        inner: '<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>',
+        outlineID: "pswp__icn-fullscreen"
+      },
+      onClick: (event, el) => {
+        if (!document.fullscreenElement) {
+          lightbox.pswp.element.requestFullscreen();
+          // Keep same ID when changing icon
+          el.querySelector('path').setAttribute('d', 'M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z');
+        } else {
+          document.exitFullscreen();
+          // Keep same ID when changing icon
+          el.querySelector('path').setAttribute('d', 'M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z');
+        }
+      }
+    });
+  });
+
   lightbox.on("change", () => {
     const target = lightbox.pswp.currSlide?.data?.element?.dataset["pswpTarget"];
     history.replaceState("", document.title, "#" + target);
